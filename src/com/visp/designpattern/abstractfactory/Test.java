7dcfee53f356149e552ba75a1c0ca7ec5f865656
victorai60 <zs497042813>
2015-07-07 17:39:57 +0800

    抽象工厂模式。

--- src/com/visp/designpattern/abstractfactory/Test.java
+++ src/com/visp/designpattern/abstractfactory/Test.java
@@ -4,6 +4,10 @@ public class Test {
 
 	// 抽象工厂模式：提供一个创建一系列相关或相互依赖对象的接口，而无需指定它们具体的类
 	public static void main(String[] args) {
-		
+		IFactory factory = new SqlServerFactory();
+		IUser user = factory.createUser();
+		user.insert(new User());
+		IDepartment department = factory.createDepartment();
+		department.getDepartment(1);
 	}
 }
